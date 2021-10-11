c80ae1d09a03331ecf8a23efdc52dea9e063debe
Amin <amin7331@yahoo.com>
2021-10-12 01:55:55 +0330

    A test for jwt in spring security

---
+++ src/main/java/amin7331/springsecuritywithjwt/model/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package amin7331.springsecuritywithjwt.model;
+
+public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
---
+++ src/test/java/amin7331/springsecuritywithjwt/SpringSecurityWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package amin7331.springsecuritywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
